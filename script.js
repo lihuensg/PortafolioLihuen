8935f5c070520ad964ac75cc23eafda330880469
lihuensg <lihuensg@gmail.com>
2023-03-13 15:53:01 -0300

    borrado de un estilo y js

--- script.js
+++ script.js
@@ -14,23 +14,23 @@ function seleccionar(){
     //oculto el menu una vez que selecciono una opcion
     document.getElementById("nav").classList = "";
     menuVisible = false;
-}
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("htmlcss");
-        habilidades[1].classList.add("js");
-        habilidades[2].classList.add("postgreSQL");
-        habilidades[3].classList.add("pascal");
-        habilidades[4].classList.add("smalltack");
-        habilidades[5].classList.add("lisp");
-        habilidades[6].classList.add("prolog");
-        habilidades[7].classList.add("mySQL");
-        habilidades[8].classList.add("cshar");
-    }
+// }
+// //Funcion que aplica las animaciones de las habilidades
+// function efectoHabilidades(){
+//     var skills = document.getElementById("skills");
+//     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+//     if(distancia_skills >= 300){
+//         let habilidades = document.getElementsByClassName("progreso");
+//         habilidades[0].classList.add("htmlcss");
+//         habilidades[1].classList.add("js");
+//         habilidades[2].classList.add("postgreSQL");
+//         habilidades[3].classList.add("pascal");
+//         habilidades[4].classList.add("smalltack");
+//         habilidades[5].classList.add("lisp");
+//         habilidades[6].classList.add("prolog");
+//         habilidades[7].classList.add("mySQL");
+//         habilidades[8].classList.add("cshar");
+//     }
 }
 
 
